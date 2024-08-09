e8c1fb701dc55d283426da26bdc18e9a7f54f5b5
Kunal Khairnar <kunal.khairnar96@gmail.com>
2024-08-09 23:14:00 +0530

    Event bus changes for graceful client shutdown

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,8 +1,8 @@
 import nats from "node-nats-streaming";
-import { randomBytes } from 'crypto';
-console.clear()
+import { randomBytes } from "crypto";
+console.clear();
 
-const stan = nats.connect("ticketing",randomBytes(4).toString('hex'), {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 stan.on("connect", () => {
