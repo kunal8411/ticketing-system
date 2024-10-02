e95c31fdb934188d8829c13973f45c542706a035
Kunal Khairnar <kunal.khairnar96@gmail.com>
2024-10-02 19:17:57 +0530

    Added listeners in ticket service for the order creatd and updated event

--- orders/src/models/Order.ts
+++ orders/src/models/Order.ts
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose';
-import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
-import { OrderStatusEnum } from '@kkticketing01/common';
-import { TicketDoc } from './Ticket';
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { OrderStatusEnum } from "@kkticketing01/common";
+import { TicketDoc } from "./Ticket";
 
 export { OrderStatusEnum };
 
@@ -41,7 +41,7 @@ const orderSchema = new mongoose.Schema(
     },
     ticket: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Ticket',
+      ref: "Ticket",
     },
   },
   {
@@ -54,13 +54,13 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
-orderSchema.set('versionKey', 'version');
+orderSchema.set("versionKey", "version");
 orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
 };
 
-const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
 
 export { Order };
