401099bb2384053bd34839aaf45b58d86608196e
Kunal Khairnar <kunal.khairnar96@gmail.com>
2024-10-02 20:20:05 +0530

    Added code if ticket is reserved then dont proceed to update that ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,24 +1,29 @@
-
-import { NotAuthirizedError, NotFoundError, RequestValidatorError, requireAuth, validateRequest } from '@kkticketing01/common';
-import express, { Request, Response, Router } from 'express';
-import { body, validationResult } from 'express-validator';
-import { Ticket } from '../models/ticket';
-import { TicketUpdatedPublisher } from '../events/publishers/TicketUpdatedPublisher';
-import { natsWrapper } from '../NatsWrapper';
-
+import {
+  BadRequestError,
+  NotAuthirizedError,
+  NotFoundError,
+  RequestValidatorError,
+  requireAuth,
+  validateRequest,
+} from "@kkticketing01/common";
+import express, { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/TicketUpdatedPublisher";
+import { natsWrapper } from "../NatsWrapper";
 
 const router = express.Router();
 
 router.put(
-  '/api/tickets/:id',
+  "/api/tickets/:id",
   requireAuth,
   [
-    body('title').not().isEmpty().withMessage('Title is required'),
-    body('price')
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
       .isFloat({ gt: 0 })
-      .withMessage('Price must be greater than 0'),
+      .withMessage("Price must be greater than 0"),
   ],
-//   validateRequest,
+  //   validateRequest,
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -28,6 +33,12 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+
+    //if ticket is reserved
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
@@ -44,7 +55,7 @@ router.put(
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
-      version: ticket.version
+      version: ticket.version,
     });
 
     res.send(ticket);
