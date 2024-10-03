4f4b8c7c002912116ae7a8490e457cd8bcb9ca0b
Kunal Khairnar <kunal.khairnar96@gmail.com>
2024-10-03 16:41:35 +0530

    Added listner in order service for the Expiration complete event

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,7 +4,7 @@ import { json } from "body-parser";
 import mongoose from "mongoose";
 import cookieSession from "cookie-session";
 
-import { errorHandler , currentUser} from "@kkticketing01/common";
+import { errorHandler, currentUser } from "@kkticketing01/common";
 import { newOrderRouter } from "./routes/new";
 import { showOrderRouter } from "./routes/show";
 import { indexOrderRouter } from "./routes/index";
@@ -12,6 +12,7 @@ import { deleteOrderRouter } from "./routes/delete";
 import { natsWrapper } from "./NatsWrapper";
 import { TicketCreatedListener } from "./events/listeners/TicketCreatedListener";
 import { TicketUpdatedListener } from "./events/listeners/TicketUpdatedListener";
+import { ExpirationCompleteListener } from "./events/listeners/ExpirationCompleteListener";
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -22,8 +23,7 @@ app.use(
   })
 );
 
-app.use(currentUser)
-
+app.use(currentUser);
 
 app.use(newOrderRouter);
 app.use(showOrderRouter);
@@ -40,37 +40,35 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined');
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined');
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
 
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined');
+    throw new Error("NATS_URL must be defined");
   }
   try {
-
-    //NATS STREAMING CONNECTION 
+    //NATS STREAMING CONNECTION
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
 
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!');
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
       process.exit();
     });
 
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
-
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
-
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     // tickets-mongo-srv is the service to connect to the tickets-mongo-depl deployment and then to docker file of mongo
     await mongoose.connect(process.env.MONGO_URI);
